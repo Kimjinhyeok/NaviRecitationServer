4263e965b185c19424f10165fbfd486a78070056
KJH <bkho@naver.com>
2021-06-09 20:10:56 +0900

    OYO 절 불러오기 적용

--- routes/recitation.js
+++ routes/recitation.js
@@ -1,6 +1,22 @@
 var router = require('express').Router();
 var pgClient = require('../utils/pgClient');
 
+router.get('/oyo/content', async (req, res) => {
+  try {
+    var { bible_code, chapter, f_verse, l_verse }=  req.query;
+
+    var query = `SELECT ARRAY_TO_STRING(array_agg(content), ' ') as content
+      FROM bible_kornkrv WHERE bible_code=${bible_code} 
+      AND chapter=${chapter} AND verse ${l_verse ? "BETWEEN " + f_verse  + " AND " + l_verse : "= " + f_verse}`;
+    
+    var pgResult = await pgClient.query(query);
+    var {content} = pgResult.rows[0];
+    
+    res.status(200).end(content);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+})
 router.get('/:category', async (req, res) => {
   try {
     var category = Number(req.params.category);
