8b7921c1ed2469a2e17833d11b9bbfd8d347d311
KJH <bkho@naver.com>
2021-06-15 13:22:12 +0900

    oyo 카드 저장 적용(MOCK 사용자 적용 중)

--- routes/recitation.js
+++ routes/recitation.js
@@ -29,16 +29,44 @@ router.get('/:category', async (req, res) => {
     var result = row.rows;
     res.status(200).send(result);
   } catch (error) {
-    res.status(error.code).send(error.message)
+    res.status(error.code).send(error.message);
   }
 });
-router.post('/oyo', (req, res) => {
+function getColumnNames(params) {
+  var columns = [];
+  var values = [];
+  for (const key in params) {
+    if (Object.hasOwnProperty.call(params, key)) {
+      columns.push(`\"${key}\"`);
+      values.push(`\'${params[key]}\'`);
+    }
+  }
+  
+  return {columns : columns.toString(), values : values.toString()};
+}
+router.post('/oyo', async (req, res) => {
   try {
-    const { theme, bible_code, chapter, f_verse, l_verse, content } = req.body;
+    var {columns, values} = getColumnNames(req.body);
+
+
+    /**
+     *  TODO
+     *  사용자 정보 MOCK 임시 데이터 적용 중
+     */
+
+    columns += ',\"owner\"';
+    values += ',\'545\'';
+
+    var query = `INSERT INTO OYO(${columns}) VALUES(${values})`;
+    var pgResult = await pgClient.query(query);
+    var rowResult = pgResult.rows[0];
+
+    res.status(200).send(rowResult);
+
   } catch (error) {
     res.status(error.code).send(error.message)
   }
-})
+});
 router.put('/', (req, res) => {
   try {
     const { memorized, cardnum, category } = req.body;
