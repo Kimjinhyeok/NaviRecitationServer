cef255bde3950e043a7261295a58c6399b3e3c22
KJH <bkho@naver.com>
2021-08-09 18:33:17 +0900

    React Router URL 이동을 위한 코드 추가(2)

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use('*', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'public/app/build'))
+  res.sendFile(path.join(__dirname, 'public/app/build/index.html'))
 });
 
 // error handler
