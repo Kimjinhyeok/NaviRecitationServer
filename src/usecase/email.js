53985d8360ad0b6c8ff23429be74583dc1cfdf1e
KJH <bkho@naver.com>
2024-06-27 17:45:33 +0900

    FIX: email 및 인증기간 jwt 토큰을 담아서 링크에 포함하도록 수정

--- src/usecase/email.js
+++ src/usecase/email.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const { sendEmail } = require('../config/stms');
 const {readFile} = require('fs/promises');
+const {privateKey} = require('../secretPrivateKey');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 const sendChangeEmail = async function(req, res, next) {
  
@@ -8,10 +12,19 @@ const sendChangeEmail = async function(req, res, next) {
 
   if(!email) throw new Error("이메일을 입력해주세요.");
 
-  const result = await readFile(path.join(__dirname, '../htmlTemplate/change_password.html'), 'utf-8');
+  const html = await readFile(path.join(__dirname, '../htmlTemplate/change_password.html'), 'utf-8');
 
-  // console.log(result.find("%URL%"));
-  const emailResult = await sendEmail("비밀번호 변경 안내", {html : result}, email);
+  const token = jwt.sign({
+    email : email,
+  }, privateKey, {
+    expiresIn : "10m"
+  }); 
+  const WEB_URL = process.env.WEB_URL;
+  
+  const PwdChangeUrl = `http://${WEB_URL}/reset/password?token=${token}`;
+
+  const convertedHtml = html.replace("#URL", PwdChangeUrl);
+  const emailResult = await sendEmail("비밀번호 변경 안내", {html : convertedHtml}, email);
 
   return emailResult
 }
